3e271de07b02a7305318ec73de137322fb3ec205
Yarik-007 <mega.yarik05@gmail.com>
2026-06-01 17:11:08 +0300

    Create P11.java

---
+++ P11.java
@@ -0,0 +1,22 @@
+//================================================================================//
+//Ограничение доступа
+//================================================================================//
+
+class Airplane{
+    int abobusov = 200;
+    public String name = "plane";
+    protected int speed = 1000;
+    private int height = 10000;
+    public int getHeight(){
+        return height;
+    }
+}
+public class P11 {
+    public static void main(String[] args){
+        Airplane plane = new Airplane();
+        System.out.println(plane.abobusov);
+        System.out.println(plane.name);
+        System.out.println(plane.speed);
+        System.out.println(plane.getHeight());
+    }
+}
