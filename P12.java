6eba58f01d4df9204bf3dbe4b9b47e7b4b764140
Yarik-007 <mega.yarik05@gmail.com>
2026-06-01 17:12:02 +0300

    Create P12.java

---
+++ P12.java
@@ -0,0 +1,26 @@
+//================================================================================//
+//Методы
+//================================================================================//
+ 
+ class calc{
+    static public int a=10, b=20;
+    int sum(int a, int b){
+        return a+b;
+    }
+    int sum(int a, int b, int c){
+        return a+b+c;
+    }
+}
+class calc1 extends calc{
+    int sum(int a, int b){
+        return a*b;
+    }
+}
+public class P12 {
+    public static void main(String[] args){
+        calc Calculator = new calc();
+        System.out.println(Calculator.sum(Calculator.a, Calculator.b));
+        calc1 Calc = new calc1();
+        System.out.println(Calc.sum(Calc.a, Calc.b));
+    }
+}
