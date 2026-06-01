8feb29c8005f5ed42fafe7ae6df2090563fd71c6
Yarik-007 <mega.yarik05@gmail.com>
2026-06-01 17:08:14 +0300

    Create P9.java

---
+++ P9.java
@@ -0,0 +1,22 @@
+//Циклы
+//Практика #1
+/*
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        //Пример бесконечного цикла while
+        while (i>0){
+            i=+1;
+        }
+
+
+        int a = 1;
+        boolean j = a >0;
+        do{
+            a+=1;
+            j = a>0;
+        }
+        while (j != false);
+    }
+}
+*/
